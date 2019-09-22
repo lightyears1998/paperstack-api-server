8e7bd7a1b757fea6a794ca87b23895d6603579cc
lightyears <lightyears1998@hotmail.com>
2019-09-22 23:28:31 +0800

    refactor: 添加User模型

---
+++ src/model/User.ts
@@ -0,0 +1,17 @@
+import sequelize from "./../db";
+import Sequelize, { Model } from "sequelize";
+
+class User extends Model {}
+
+User.init({
+  email: {
+    type:      Sequelize.STRING,
+    allowNull: false
+  },
+  passwordHash: {
+    type:      Sequelize.STRING,
+    allowNull: false
+  }
+}, { sequelize });
+
+export default User;

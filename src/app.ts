bafc90810b1a111eee1b69cb1937f71a85f0ce74
lightyears <lightyears1998@hotmail.com>
2019-09-22 22:21:36 +0800

    feat: 添加默认路由

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,19 @@ import path from 'path';
 import config from "./config";
 import express from "express";
 
+const version = JSON.parse(fs.readFileSync(path.resolve(`${__dirname}/../package.json`), 'utf8')).version
 const serverConfig = config.server;
 
 const app = express();
-app.listen(serverConfig.port, () => {
-  const version = JSON.parse(fs.readFileSync(path.resolve(`${__dirname}/../package.json`), 'utf8')).version
 
+app.all('/', (req, res) => {
+  res.json({
+    "msg": "欢迎使用PaperStack API Server。",
+    "version": version
+  })
+})
+
+app.listen(serverConfig.port, () => {
   console.log(`PaperStack API服务器 v${version}`)
   console.log(`正在监听${serverConfig.port}端口。`)
 });

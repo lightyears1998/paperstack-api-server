062acb2f54cfa2b0f098eb3d6b1b039528750ef5
lightyears <lightyears1998@hotmail.com>
2019-09-24 16:37:59 +0800

    fix: 在使用参数前执行类型检查

--- src/route/check-email.ts
+++ src/route/check-email.ts
@@ -5,7 +5,7 @@ import { checkEmailAvailabilty } from "./../util";
  * 检查由`req.body.email`指定邮箱地址是否未被占用。
  */
 const checkEmail: RequestHandler = async function (req, res) {
-  let email: string = req.body.email || "";
+  let email: string = typeof req.body.email === "string" ? req.body.email : "";
 
   res.json({
     available: await checkEmailAvailabilty(email)

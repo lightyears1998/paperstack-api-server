404572c24a51ef4be1cdbcf9ded2f4606a94fdcc
lightyears <lightyears1998@hotmail.com>
2019-09-24 23:13:07 +0800

    feat: 实现logout路由

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,6 +1,7 @@
 import checkEmail from "./check-email";
 import register from "./register";
 import login from "./login";
+import logout from "./logout";
 import modifyPassword from "./modify-password";
 
 import { Router } from "express";
@@ -12,6 +13,7 @@ const router = Router();
 router.post("/check/email", checkEmail);
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
 router.post("/modify/password", modifyPassword);
 
 export default router;
---
+++ src/route/logout.ts
@@ -0,0 +1,18 @@
+import { Authkey } from "../model";
+import { RequestHandler } from "express";
+
+
+const logout: RequestHandler = async function (req, res) {
+  let authkey: string = typeof req.body.authkey === "string" ? req.body.authkey : "";
+
+  Authkey.destroy({
+    where: {
+      value: authkey
+    }
+  });
+
+  res.json({});
+};
+
+
+export default logout;

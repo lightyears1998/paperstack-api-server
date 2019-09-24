792eaa07bab65b8c77634576d763645e61927c48
lightyears <lightyears1998@hotmail.com>
2019-09-24 19:25:30 +0800

    feat: 新用户注册

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,9 +1,12 @@
-import { Router } from "express";
 import checkEmail from "./check-email";
+import register from "./register";
+
+import { Router } from "express";
+
 
 const router = Router();
 
 router.post("/check/email", checkEmail);
-
+router.post("/register", register);
 
 export default router;
---
+++ src/route/register.ts
@@ -0,0 +1,43 @@
+import { checkEmailAvailabilty, hashPassword } from "./../util";
+
+import { RequestHandler } from "express";
+import { User } from "../model";
+
+/**
+ * 注册新用户
+ */
+const register: RequestHandler = async function (req, res) {
+  let email: string = typeof req.body.email === "string" ? req.body.email : "";
+  let password: string = typeof req.body.password === "string" ? req.body.password : "";
+
+  /*
+   * @TODO
+   * 验证邮箱地址格式（validator）
+   */
+
+  if (email === "" || password === "") {
+    res.json({
+      message: "邮箱或密码不能为空"
+    });
+    return;
+  }
+
+  let emailOk = checkEmailAvailabilty(email);
+  if (!emailOk) {
+    res.json({
+      message: "注册失败"
+    });
+    return;
+  }
+
+  await User.create({
+    email:        email,
+    passwordHash: await hashPassword(password)
+  });
+  res.json({
+    message: "注册成功"
+  });
+};
+
+
+export default register;

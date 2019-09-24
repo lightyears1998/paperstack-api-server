2b7bd0e27bb70e3fcbd3b8aea6dbadcdbbe06654
lightyears <lightyears1998@hotmail.com>
2019-09-24 15:54:48 +0800

    refactor: 移除body-parser

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,12 @@ import router from "./route";
 import fs from "fs";
 import path from "path";
 import express from "express";
-import bodyParser from "body-parser";
 
 const version = JSON.parse(fs.readFileSync(path.resolve(`${__dirname}/../package.json`), "utf8")).version;
 const serverConfig = config.server;
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.all("/", (req, res) => {
   res.json({
